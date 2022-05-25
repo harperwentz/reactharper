16fa3404600a82a6a0a115fb7dfd6efc64cea2bb
harperwentz <104076178+harperwentz@users.noreply.github.com>
2022-05-25 12:04:59 -0500

    playing around with style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-function App(){
+import React, {useState} from "react";
+import Tweet from "./Tweet";
+import "./App.css";
 
-  const sayHello = () => {
-    console.log('hello');
-  };
+function App(props){
 
-  
 
-  return(
-    <div>
-      <h1>Hello React</h1>
-      <button onClick={sayHello}>Hello</button>
-    </div>
-  );
+
+
+return(
+  <div className="todoapp">
+    <h1>To Do List</h1>
+    <div className=""
+    {users.map(user =>(
+      <Tweet name={user.name} message ={user.message}  />
+    ))}
+    <h1 className={isRed ? "red" : ""}>Change my color!</h1>
+    <button onClick={increment}>Increment</button>
+    <h1>{count}</h1>
+    
+  </div>
+);
 }
+const [users, setUser] = useState([
+{name: "Harper", message: 'Hello there'},
+{name: 'Jacob', message: 'i am yaycob'},
+{name: 'maddy', message: 'i am moozer'}
+]);
+const [isRed, setRed] = useState(false);
+const [count, setCount] = useState(0);
 
+
+
+const increment = () => {
+setCount(count + 1)
+setRed(!isRed);
+};
 export default App;
