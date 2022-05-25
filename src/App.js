ebb03de1598bd175560afb060b6b7dcdfbacc497
harperwentz <104076178+harperwentz@users.noreply.github.com>
2022-05-25 12:10:45 -0500

    starting to do list header

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import React, {useState} from "react";
-import Tweet from "./Tweet";
 import "./App.css";
 
 function App(props){
 
-
-
-
-return(
-  <div className="todoapp">
-    <h1>To Do List</h1>
-    <div className=""
-    {users.map(user =>(
-      <Tweet name={user.name} message ={user.message}  />
-    ))}
-    <h1 className={isRed ? "red" : ""}>Change my color!</h1>
-    <button onClick={increment}>Increment</button>
-    <h1>{count}</h1>
-    
-  </div>
-);
+  return(
+    <div className="todoapp">
+      <header>
+        <h1>Harper's Todo List</h1>
+      </header>
+    </div>
+  );
 }
-const [users, setUser] = useState([
-{name: "Harper", message: 'Hello there'},
-{name: 'Jacob', message: 'i am yaycob'},
-{name: 'maddy', message: 'i am moozer'}
-]);
-const [isRed, setRed] = useState(false);
-const [count, setCount] = useState(0);
 
-
-
-const increment = () => {
-setCount(count + 1)
-setRed(!isRed);
-};
 export default App;
